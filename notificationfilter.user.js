81fd79079d3e705ef20a7cf88f93ac1ba7060264
Jose Castaneda <jomcastaneda@gmail.com>
2018-08-24 10:25:19 -0700

    Initial commit
    This is the first commit for the script.

---
+++ notificationfilter.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        GitHub notification filter
+// @namespace   https://blog.josemcastaneda.com
+// @version     0.1.0
+// @description Shows a small UI that toggles GitHub issue notifications.
+// @author      Jose Castaneda
+// @match       https://github.com/notifications
+// ==/UserScript==
+//
+
+(function() {
+	'use strict';
+	// Our code goes here.
+})();
+
